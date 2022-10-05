fdf9884631d32608a940a3b4c654ee0a78a689ab
Hailey Ahn <haa40@sfu.ca>
2022-10-04 20:59:57 -0700

    Implemented scatter map

---
+++ SEM.py
@@ -0,0 +1,134 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SEM_agent(object):
+  def __init__(self, agent_config, initial_location):
+    self.resolution = agent_config.sensor_specifications[0].resolution
+    
+    # Init location info
+    self.agent_location = np.array([0,0,0],np.float32)
+    self.initial_location = initial_location[0:2]
+    self.elevation = initial_location[2] # TODO: Verify!!
+    self.camera_height = agent_config.sensor_specifications[0].position[2] # TODO: Verify!!
+
+    # Init map related location info
+    self.map_center = np.array([5.0,5.0]); # TODO: Fix this to be middle of drawn map
+
+    # Init camera info
+    self.res_width = self.resolution[0]
+    self.res_height = self.resolution[1]
+    self.camera_hfov = agent_config.sensor_specifications[0].hfov
+    self.camera_xc = (self.res_width-1.0) / 2
+    self.camera_zc = (self.res_height-1.0) / 2
+    # TODO: replace 90 with self.camera_hfov.
+    self.camera_f = (self.res_width / 2.) / np.tan(np.deg2rad(90/ 2.))
+    
+    self.all_marked_points = np.array([])
+    self.all_agent_marks = self.map_center.reshape(1,2)
+
+    self.done = False
+    self.display_test_figs = False
+    ## TODO: Fill this.
+
+  def act(self, obs, theta, location, done):
+    depth = obs['depth']
+    self.done = done
+    self.update_agent_location(theta, location)
+    coords = self.unproject_to_world(depth)
+    self.add_to_map(coords)
+
+  def update_agent_location(self, theta, location):
+    self.agent_location[0:2] = self.map_center + (location - self.initial_location)
+    self.agent_location[2] = theta
+
+  def unproject_to_world(self, depth):
+    # Filter out errors
+    depth = depth[:,:,0]*1
+    depth[depth==0] = np.NaN
+    camera_coords = self.depth2camera(depth)
+    geocentric_coords = self.camera2geocentric(camera_coords)
+    world_coords = self.geocentric2world(geocentric_coords)
+    return world_coords
+  
+  def add_to_map(self, coords):
+    # Slice where Y is between -1 and 1.
+    sliced_coords = coords[coords[:,:,2]>-1]
+    sliced_coords = sliced_coords[sliced_coords[:,2]<1]
+    
+    if (self.all_marked_points.size == 0):
+      self.all_marked_points = sliced_coords
+    else:
+      self.all_marked_points = np.concatenate((self.all_marked_points, sliced_coords), axis=0)
+
+    self.all_agent_marks = np.concatenate((self.all_agent_marks, self.agent_location[0:2].reshape(1,2)), axis=0)
+    
+    #ax.azim = 0
+    #ax.elev = 0
+    if (self.display_test_figs or self.done):
+      fig = plt.figure()
+      plt.scatter(self.all_marked_points[:,0], self.all_marked_points[:,1], marker='.')
+      plt.scatter(self.all_agent_marks[:,0], self.all_agent_marks[:,1], marker='*', color='gray')
+      plt.scatter(self.agent_location[0], self.agent_location[1], marker='*', color='red')
+      plt.show()
+    print("---")
+
+  def depth2camera(self, depth):
+    # TODO: Rewrite and organize.
+    x, z = np.meshgrid(np.arange(depth.shape[-1]), np.arange(depth.shape[-2]-1, -1, -1))
+    X = (x-self.camera_xc) * depth / self.camera_f
+    Z = (z-self.camera_zc) * depth / self.camera_f
+    XYZ = np.concatenate((X[...,np.newaxis], depth[...,np.newaxis], Z[...,np.newaxis]), axis=X.ndim)
+    return XYZ
+
+    """fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(X, depth, Z, marker='.')
+    ax.set_label('X')
+    ax.set_label('Y = depth')
+    ax.set_label('Z')
+    plt.show()"""
+
+  def camera2geocentric(self, camera_coords):
+    # Building rotation matrix
+    R = self.get_rotation_matrix([1.,0.,0.], angle=np.deg2rad(self.elevation))
+
+    geocentric_coords = np.matmul(camera_coords.reshape(-1,3), R.T).reshape(camera_coords.shape)
+    geocentric_coords[...,2] = geocentric_coords[...,2] + self.camera_height
+    return geocentric_coords
+
+  def geocentric2world(self, geocentric_coords):
+    # TODO: Fix this part
+    R = self.get_rotation_matrix([0.,0.,1.], angle=self.agent_location[2]-np.pi/2.)
+    world_coord = np.matmul(geocentric_coords.reshape(-1,3), R.T).reshape(geocentric_coords.shape)
+    world_coord[:,:,0] = world_coord[:,:,0] + self.agent_location[0]
+    world_coord[:,:,1] = world_coord[:,:,1] + self.agent_location[1]
+    
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.azim = 0
+    ax.elev = 0
+    ax.scatter(world_coord[:,:,0], world_coord[:,:,1], world_coord[:,:,2], marker='.', s=0.05)
+    ax.set_label('X')
+    ax.set_label('Y = depth')
+    ax.set_label('Z')
+    plt.show()"""
+    return world_coord
+
+  def get_rotation_matrix(self, ax_, angle):
+    # TODO: Fix everything here
+    ANGLE_EPS = 0.001
+    ax = self.normalize(ax_)
+    if np.abs(angle) > ANGLE_EPS:
+      S_hat = np.array(
+          [[0.0, -ax[2], ax[1]], [ax[2], 0.0, -ax[0]], [-ax[1], ax[0], 0.0]],
+          dtype=np.float32)
+      R = np.eye(3) + np.sin(angle)*S_hat + \
+          (1-np.cos(angle))*(np.linalg.matrix_power(S_hat, 2))
+    else:
+      R = np.eye(3)
+    return R
+
+
+  def normalize(self, v):
+    return v / np.linalg.norm(v)
---
+++ example_agent.py
@@ -0,0 +1,82 @@
+import os
+import habitat_sim
+import SEM
+import cv2 as _cv2
+import numpy as np
+import random
+import quaternion
+
+display = False
+
+# Setting up display
+if (display):
+  _cv2.namedWindow("sensor_depth")
+
+# Setting up backeng config
+backend_cfg = habitat_sim.SimulatorConfiguration()
+# Specifying scene path
+backend_cfg.scene_id = (
+  "../map_plan_baseline_modified/data/scene_datasets/habitat-test-scenes/skokloster-castle.glb"
+)
+
+# Setting up sensor
+depth_sensor = habitat_sim.CameraSensorSpec()
+depth_sensor.uuid = "depth"
+depth_sensor.resolution = [512, 512]
+depth_sensor.position = 1.5 * habitat_sim.geo.UP
+depth_sensor.sensor_type = habitat_sim.SensorType.DEPTH
+#depth_sensor.far = 100.0
+
+# Setting up agent config
+agent_config = habitat_sim.AgentConfiguration()
+agent_config.action_space = {
+    "move_forward": habitat_sim.agent.ActionSpec(
+        "move_forward", habitat_sim.agent.ActuationSpec(amount=0.25)
+    ),
+    "turn_left": habitat_sim.agent.ActionSpec(
+        "turn_left", habitat_sim.agent.ActuationSpec(amount=30.0)
+    ),
+    "turn_right": habitat_sim.agent.ActionSpec(
+        "turn_right", habitat_sim.agent.ActuationSpec(amount=30.0)
+    ),
+}
+agent_config.sensor_specifications = [depth_sensor]
+
+sim = habitat_sim.Simulator(habitat_sim.Configuration(backend_cfg, [agent_config]))
+
+topdown_position = sim.last_state().position[1:3]
+location = np.zeros(3)
+location[0] = topdown_position[1] * (-1)
+location[1] = topdown_position[0] * (-1)
+location[2] = sim.last_state().position[0]
+
+semantic_agent = SEM.SEM_agent(agent_config, location)
+
+
+def _action(sim):
+  num_acts = 100
+  for act_no in range(num_acts):
+    action_code = random.randint(0,2)
+    if action_code == 0:
+        obs = sim.step("move_forward")
+    elif action_code == 1:
+        obs = sim.step("turn_left")
+    elif action_code == 2:
+        obs = sim.step("turn_right")
+    print("action_code: ", action_code)
+
+    theta = quaternion.as_rotation_vector(sim.last_state().rotation)
+    theta = theta[1]
+
+    topdown_position = sim.last_state().position[1:3]
+    location = np.zeros(2)
+    location[0] = topdown_position[1] * (-1)
+    location[1] = topdown_position[0] * (-1)
+    print("retrieved position: ", sim.last_state().position)
+
+    # TODO: Get rid of this.
+    obs["depth"] = obs["depth"][:,:,np.newaxis]
+
+    semantic_agent.act(obs, theta, location, (act_no == num_acts-1))
+
+_action(sim)
